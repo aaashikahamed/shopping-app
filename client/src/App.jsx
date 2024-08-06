288265222f865cc00720f545c8b70cd8530d82f7
aaashikahamed <ahamedaashik071@gmail.com>
2024-08-06 20:11:15 +0530

    made changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
-import Header from './components/Header'
 
 function App() {
 
 
   return (
     <BrowserRouter>
-      <Header />
+      <Home />  
     </BrowserRouter>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@
 
 //  import Images
 import React from 'react'
-import banner from '../assets/images/banner.jpg'
+import hero from '../assets/images/banner.jpg'
 
 function Home(){
     return (
@@ -12,15 +12,15 @@ function Home(){
         <div>
           <div className="relative">
             <img
-              src={banner}
+              src={hero}
               alt=""
-              className="h-[500px] w-full object-right sm:object-center object-cover "
+              className="h-32 w-full object-right sm:object-center object-cover "
             />
-            <div className="absolute top-40 start-10 sm:start-[20rem] md:start-[30rem] lg:start-[40rem] xl:start-[50rem]">
+            <div className="absolute top-40 start-10 sm:start-[22rem] md:start-[32rem] lg:start-[42rem] xl:start-[52rem]">
               <p className="text-3xl font-bold text-white">
                
               </p>
-              <button className="bg-black text-white font-semibold px-5 py-2 rounded-md mt-6 hover:bg-gray-800">
+              <button className="bg-black text-white font-bold px-5 py-2 rounded-md mt-6 hover:bg-gray-700">
                 View Products
               </button>
             </div>
@@ -33,15 +33,14 @@ function Home(){
 
     <div className="max-w-6xl mx-auto p-2">
         <div className='text-center mt-10'>
-          <h6 className='uppercase font-bold text-sm'>Top sale on this month</h6>
-          <h1 className='text-3xl font-semibold mt-2 text-[#3d52a0] font-serif'>Trending Products</h1>
+          <h1 className='text-3xl font-bold mt-2 text-[#3d52a0] font-serif'>Trending Products</h1>
         </div>
 
 
 
         <div className='flex flex-wrap gap-5 mt-10 '>
             <div  className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-[140px] sm:w-[250px]'>
-              <img src={banner} alt="" className='h-[200px] sm:h-[250px] w-[200px] sm:w-[250px] object-cover hover:scale-105 transition-scale duration-300'/>
+              <img src={hero} alt="" className='h-12 sm:h-[250px] w-12 sm:w-[250px] object-cover hover:scale-105 transition-scale duration-300'/>
               <div className='p-3 flex flex-col gap-2 w-full mt-3'>
                 <h3 className='text-sm sm:text-lg font-semibold text-slate-700 line-clamp-1'>Product Name</h3>
                 <p className='text-xs sm:text-sm text-gray-600 line-clamp-2'>Product Description</p>
