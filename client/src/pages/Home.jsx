01009ad5bc848264e8fb42776471d362734aba89
aaashikahamed <ahamedaashik071@gmail.com>
2024-08-06 20:18:32 +0530

    made changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,11 +14,11 @@ function Home(){
             <img
               src={hero}
               alt=""
-              className="h-32 w-full object-right sm:object-center object-cover "
+              className="h-[500px] w-full object-right sm:object-center object-cover "
             />
             <div className="absolute top-40 start-10 sm:start-[22rem] md:start-[32rem] lg:start-[42rem] xl:start-[52rem]">
               <p className="text-3xl font-bold text-white">
-               
+                Welcome To Our Store
               </p>
               <button className="bg-black text-white font-bold px-5 py-2 rounded-md mt-6 hover:bg-gray-700">
                 View Products
@@ -64,7 +64,7 @@ function Home(){
         </div>
         <div className='flex flex-wrap gap-5 mt-10'>
             <div  className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-[140px] sm:w-[250px]' >
-              <img src={banner} alt="" className='h-[200px] sm:h-[250px] w-[200px] sm:w-[250px] object-cover hover:scale-105 transition-scale duration-300'/>
+              <img src={hero} alt="" className='h-[200px] sm:h-[250px] w-[200px] sm:w-[250px] object-cover hover:scale-105 transition-scale duration-300'/>
               <div className='p-3 flex flex-col gap-2 w-full mt-3'>
                 <h3 className='text-sm sm:text-lg font-semibold text-slate-700 line-clamp-1'>Offer Product Name</h3>
                 <p className='text-xs sm:text-sm text-gray-600 line-clamp-2'>Offer Product Description</p>
