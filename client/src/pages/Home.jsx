a159efb58480aab7e220004f51626bd4dff7e014
aaashikahamed <ahamedaashik071@gmail.com>
2024-08-06 12:46:50 +0530

    Added Trending Products Section in Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -28,11 +28,39 @@ function Home(){
         </div>
 
         {/* Trending Products goes here */}
-        <div className=" flex flex-wrap gap-5 mt-10">
-            <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-9 sm:w-16 '>
-
+        <div className='flex flex-wrap gap-5 mt-10 ml-10 '>
+            <div  className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-[140px] sm:w-[250px]'>
+              <img src={banner} alt="" className='h-[200px] sm:h-[250px] w-[200px] sm:w-[250px] object-cover hover:scale-105 transition-scale duration-300'/>
+              <div className='p-3 flex flex-col gap-2 w-full mt-3'>
+                <h3 className='text-sm sm:text-lg font-semibold text-slate-700 line-clamp-1'>Product Name</h3>
+                <p className='text-xs sm:text-sm text-gray-600 line-clamp-2'>Product Description</p>
+                
+                  <div className='flex flex-col sm:flex-row sm:items-center gap-2'>
+                    <p className='text-red-600 line-through text-sm'>200/=</p>
+                    <p className='text-green-600 font-semibold'>100/=</p>
+                  </div>
+                    <p className='font-semibold'>100/=</p>
+              </div>
             </div>
         </div>
+
+        <div className='flex flex-wrap gap-5 mt-10 ml-10 '>
+            <div  className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-[140px] sm:w-[250px]'>
+              <img src={banner} alt="" className='h-[200px] sm:h-[250px] w-[200px] sm:w-[250px] object-cover hover:scale-105 transition-scale duration-300'/>
+              <div className='p-3 flex flex-col gap-2 w-full mt-3'>
+                <h3 className='text-sm sm:text-lg font-semibold text-slate-700 line-clamp-1'>Product Name</h3>
+                <p className='text-xs sm:text-sm text-gray-600 line-clamp-2'>Product Description</p>
+                
+                  <div className='flex flex-col sm:flex-row sm:items-center gap-2'>
+                    <p className='text-red-600 line-through text-sm'>200/=</p>
+                    <p className='text-green-600 font-semibold'>100/=</p>
+                  </div>
+                    <p className='font-semibold'>100/=</p>
+              </div>
+            </div>
+        </div>
+
+        {/* Trending Products ends here */}
       </React.Fragment>
     );
 }
