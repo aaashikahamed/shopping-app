7db6ba6c68ff4afad1876931fc71659814e4e969
aaashikahamed <ahamedaashik071@gmail.com>
2024-08-07 21:16:12 +0530

    Made Few Changes and Uplodaed Project Pre Planning

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
 
 function App() {
 
 
   return (
     <BrowserRouter>
-      <Home />  
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
     </BrowserRouter>
   )
 }
---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,15 @@
+import {Link, useNavigate} from "react-router-dom";
+import {useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+
+function SignIn(){
+    
+
+    return(
+        
+    )
+
+
+}
+
+export default SignIn
